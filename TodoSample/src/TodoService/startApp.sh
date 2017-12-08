fe931b26c4dfc3c97e091687aac491c491cc71c1
skjagini <skjagini@gmail.com>
2017-12-07 19:07:12 -0500

    Added appspec.yml and startApp.sh

---
+++ TodoSample/src/TodoService/startApp.sh
@@ -0,0 +1,13 @@
+﻿#!/bin/sh
+echo "PWD"
+echo $PWD
+
+sudo rm -rf /Musiq
+
+sudo service nginx stop
+sudo service supervisor stop
+sudo service supervisor start  #dotnet /Musiq/HelloWorldApiCore.dll 
+sudo service nginx start
+
+exit 0
+
